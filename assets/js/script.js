70c34fc622506285780921e8c26a89d470e0f777
thhalessouza <thais-ssouza@outlook.com>
2021-09-21 00:08:52 -0300

    implementação js

---
+++ assets/js/script.js
@@ -0,0 +1,84 @@
+let nome = window.document.getElementById("nome")
+let email = document.querySelector("#email")
+let assunto = document.querySelector("#assunto")
+let nomeOk = false
+let emailOk = false
+let assuntoOk = false
+let mapa = document.querySelector("#mapa")
+
+nome.style.width = "100%"
+email.style.width = "100%"
+
+
+function validaNome () {
+    let txt = document.querySelector("#txtNome")
+    if (nome.value.length < 3) {
+    txt.innerHTML = "Nome Inválido!"
+    txt.style.color = "red"
+    } else {
+      txt.innerHTML = "Nome Válido!"
+      txt.style.color = "green"
+      nomeOk = true 
+    }
+
+
+    }
+
+    function validaEmail () {
+    let txtEmail = document.querySelector("#txtEmail")
+
+
+    if (email.value.indexOf("@") == -1 || email.value.indexOf(".") == -1) {
+      txtEmail.innerHTML = "Email Inválido!"
+      txtEmail.style.color = "red"
+    }else {
+        txtEmail.innerHTML = "Email Válido!"
+        txtEmail.style.color = "green"
+        emailOk = true 
+    }
+
+    }
+
+
+    function validaAssunto() {
+    let txtAssunto = document.querySelector("#txtAssunto")
+
+    if (assunto.value.length >=100) {
+    txtAssunto.innerHTML = "Digite no máximo 100 caracteres"
+    txtAssunto.style.color = "red"
+    txtAssunto.style.display = "block"
+
+    }else {
+         txtAssunto.style.display = "none"
+         assuntoOk = true 
+
+
+    }
+
+}
+
+function enviar() {
+if (nomeOk == true && emailOk == true && assuntoOk == true){
+alert ("Formulário Enviado com Sucesso!")
+
+}else {
+alert ("Preencha o formulário corretamente antes de enviar!")
+
+}
+
+
+}
+
+function mapaZoom() {
+mapa.style.width = "800px"
+mapa.style.height = "600px"
+
+}
+
+function mapaNormal() {
+    mapa.style.width = "400px"
+    mapa.style.height = "250px"
+
+}
+
+
